81d25eea3230e0c9d03ab88ce9ad544877686690
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2022-10-28 18:01:26 +0200

    enhancement: Take latest changes from funders dashboard

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Footer from './Footer';
 import Header from './Header';
@@ -7,11 +8,17 @@ interface Props {
   children: React.ReactNode;
 }
 
+const Main = styled.main`
+  background: ${props => props.theme.colors.black[50]};
+  min-height: 380px;
+  padding: 48px max(8px, calc((100vw - 1280px) / 2));
+`;
+
 export default function Layout({ children }: Props) {
   return (
     <React.Fragment>
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
       <Footer />
     </React.Fragment>
   );
