7391bab201e9d3624fb9da9f3339226f456f8fd7
Gustav Larsson <gustav.larsson@gmail.com>
2022-11-09 13:04:00 +0100

    Add globals.css file (styled-component's injectGlobal causes style recalculation and font loading on every interaction)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,123 +4,12 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import NextNProgress from 'nextjs-progressbar';
 import { IntlProvider } from 'react-intl';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import { useApollo } from '../lib/apollo-client';
 import theme from '@opencollective/frontend-components/lib/theme';
 
-const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-Regular.woff2") format("woff2"),
-        url("/fonts/inter/Inter-Regular.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  italic;
-    font-weight: 400;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-Italic.woff2") format("woff2"),
-        url("/fonts/inter/Inter-Italic.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 500;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-Medium.woff2") format("woff2"),
-        url("/fonts/inter/Inter-Medium.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  italic;
-    font-weight: 500;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-MediumItalic.woff2") format("woff2"),
-        url("/fonts/inter/Inter-MediumItalic.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 600;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-SemiBold.woff2") format("woff2"),
-        url("/fonts/inter/Inter-SemiBold.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  italic;
-    font-weight: 600;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-SemiBoldItalic.woff2") format("woff2"),
-        url("/fonts/inter/Inter-SemiBoldItalic.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 700;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-Bold.woff2") format("woff2"),
-        url("/fonts/inter/Inter-Bold.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  italic;
-    font-weight: 700;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-BoldItalic.woff2") format("woff2"),
-        url("/fonts/inter/Inter-BoldItalic.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 800;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-ExtraBold.woff2") format("woff2"),
-        url("/fonts/inter/Inter-ExtraBold.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  italic;
-    font-weight: 800;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-ExtraBoldItalic.woff2") format("woff2"),
-        url("/fonts/inter/Inter-ExtraBoldItalic.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 900;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-Black.woff2") format("woff2"),
-        url("/fonts/inter/Inter-Black.woff") format("woff");
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style:  italic;
-    font-weight: 900;
-    font-display: swap;
-    src: url("/fonts/inter/Inter-BlackItalic.woff2") format("woff2"),
-        url("/fonts/inter/Inter-BlackItalic.woff") format("woff");
-  }
-
-  body {
-    font-family: 'Inter', sans-serif;
-    padding: 0 1rem 1rem 1rem;
-    max-width: 1200px;
-    margin: 0 auto;
-    background: #f9fafb;
-    color: #333;
-  }
-
-  * {
-    font-family: 'Inter', sans-serif;
-    box-sizing: border-box;
-  }
-`;
+import '../globals.css';
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -131,7 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <IntlProvider locale="en">
       <ApolloProvider client={apolloClient}>
         <ThemeProvider theme={theme}>
-          <GlobalStyles />
           <NextNProgress />
           <Component {...pageProps} />
         </ThemeProvider>
